499321f975752bacc8d3835558fe1fc269232260
Miuroro <monera.mual@gmail.com>
2026-08-18 16:20:17 +0200

    chore: added initial USER database schema migration

---
+++ backend/src/main/resources/db/migration/schema_V1.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE user_credentials (
+    user_id UUID PRIMARY KEY,
+    password_hash VARCHAR(255) NOT NULL,
+    refresh_token TEXT,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT fk_user_credentials FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_profiles (
+    user_id UUID PRIMARY KEY,
+    prefered_role VARCHAR(255),
+    prefered_city VARCHAR(255),
+    prefered_country VARCHAR(255),
+    education_level VARCHAR(255),
+    experience_level VARCHAR(255),
+    salary NUMERIC(10, 2),
+    skills JSONB,
+    CONSTRAINT fk_user_profile FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TYPE job_state AS ENUM (
+    'APPLIED',
+    'REJECTED',
+    'ACCEPTED',
+    'DECLINED'
+);
+
+CREATE TABLE saved_jobs (
+    user_id UUID NOT NULL,
+    job_id INT NOT NULL,
+    saved_status VARCHAR(50),
+    job_state job_state,
+    PRIMARY KEY (user_id, job_id),
+    CONSTRAINT fk_saved_jobs_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
